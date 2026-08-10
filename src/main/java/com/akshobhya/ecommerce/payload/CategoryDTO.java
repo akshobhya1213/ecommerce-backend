997781d5c6bcbcbf7c1c23a719f0d55b3ba89ae6
akshobhya <bgmiak2005@gmail.com>
2026-08-10 22:42:03 +0530

    Update CategoryDTO.java

--- src/main/java/com/akshobhya/ecommerce/payload/CategoryDTO.java
+++ src/main/java/com/akshobhya/ecommerce/payload/CategoryDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
-    private Long categoryId;
+    
+    private Long categoryid;
     private String categoryName;
 }
