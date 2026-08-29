fb553c439174e3772e3adfedad3c70e4f70cd796
akshobhya <bgmiak2005@gmail.com>
2026-08-29 19:16:09 +0530

    Remove OrderItemRepository interface

--- src/main/java/com/akshobhya/ecommerce/repositories/OrderItemRepository.java
+++ src/main/java/com/akshobhya/ecommerce/repositories/OrderItemRepository.java
@@ -1,9 +1,1 @@
-package com.akshobhya.ecommerce.repositories;
 
-import com.akshobhya.ecommerce.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
