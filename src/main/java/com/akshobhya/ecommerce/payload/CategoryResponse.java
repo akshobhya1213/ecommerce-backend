82d05512738154b6048c87d72eac2430d82c1efd
akshobhya <bgmiak2005@gmail.com>
2026-09-01 23:00:32 +0530

    Update CategoryResponse.java

--- src/main/java/com/akshobhya/ecommerce/payload/CategoryResponse.java
+++ src/main/java/com/akshobhya/ecommerce/payload/CategoryResponse.java
@@ -7,13 +7,5 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CategoryResponse {
-    private List<CategoryDTO> content;
-    private Integer pageNumber;
-    private Integer pageSize;
-    private Long totalElements;
-    private Integer totalPages;
-    private boolean lastPage;
+ lastPage;
 }
