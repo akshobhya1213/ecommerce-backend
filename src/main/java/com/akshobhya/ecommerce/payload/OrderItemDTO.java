9e47ffa5a140448283b4a9022fc86b67f9592854
akshobhya <bgmiak2005@gmail.com>
2026-09-03 22:38:49 +0530

    Update OrderItemDTO.java

--- src/main/java/com/akshobhya/ecommerce/payload/OrderItemDTO.java
+++ src/main/java/com/akshobhya/ecommerce/payload/OrderItemDTO.java
@@ -4,3 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long orderItemId;
+    private ProductDTO product;
+    private Integer quantity;
+    private Double discount;
+    private Double orderedProductPrice;
+}
