971610fab4b22b0362fa972d036a459a7d994cbd
akshobhya <bgmiak2005@gmail.com>
2026-09-03 22:39:34 +0530

    Remove unused fields from AddressDTO
    Removed unnecessary fields from AddressDTO.

--- src/main/java/com/akshobhya/ecommerce/payload/AddressDTO.java
+++ src/main/java/com/akshobhya/ecommerce/payload/AddressDTO.java
@@ -13,14 +13,5 @@ public class AddressDTO {
 
     private String street;
 
-    private String buildingName;
-
-    private String city;
-
-    private String state;
-
-    private String country;
-
-    private String pincode;
 
 }
