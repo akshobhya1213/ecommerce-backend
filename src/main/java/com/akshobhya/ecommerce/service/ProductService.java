9f03539efde4ee8d5407e0f3ed5afb0489bc2cc5
akshobhya <bgmiak2005@gmail.com>
2026-09-24 17:57:49 +0530

    Add searchByCategory method to ProductService interface

--- src/main/java/com/akshobhya/ecommerce/service/ProductService.java
+++ src/main/java/com/akshobhya/ecommerce/service/ProductService.java
@@ -10,7 +10,7 @@ public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO product);
 
     ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
-
+  
     ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
     ProductResponse searchProductByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
